9b1105151c46aecfffafe78aea01d201ecb23e1e
dstopka <dariusz.marek.stopka@gmail.com>
2020-03-31 10:56:29 +0200

    [01_toolbat] Added countries toolbar

--- excercises/ex_01_toolbar/Resource.h
+++ excercises/ex_01_toolbar/Resource.h
@@ -2,18 +2,24 @@
 // Microsoft Visual C++ generated include file.
 // Used by ex_01_toolbar.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDP_OLE_INIT_FAILED			100
-#define IDR_MAINFRAME				128
-#define IDR_ex_01_toolbarTYPE				130
+#define IDR_COUNTRIES_BAR               4
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_ex_01_toolbarTYPE           130
+#define ID_COUNTRIES_JAMAJKA            32771
+#define ID_COUNTRIES_JAPAN              32772
+#define ID_JAMAJKA                      32773
+#define ID_JAPAN                        32774
+#define ID_BUTTON32780                  32780
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
