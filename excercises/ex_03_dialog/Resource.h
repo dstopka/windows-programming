4392f516b7248b56fdee1d10531d913f53c407e6
dstopka <dariusz.marek.stopka@gmail.com>
2020-04-21 17:41:52 +0200

    [ex_03_dialog] Added dialog window layout

--- excercises/ex_03_dialog/Resource.h
+++ excercises/ex_03_dialog/Resource.h
@@ -2,18 +2,32 @@
 // Microsoft Visual C++ generated include file.
 // Used by ex_03_dialog.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDP_OLE_INIT_FAILED			100
-#define IDR_MAINFRAME				128
-#define IDR_dialogTYPE				130
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_dialogTYPE                  130
+#define IDD_TEST                        310
+#define IDC_DATANUM                     1001
+#define IDC_FIRST                       1002
+#define IDC_THIRD                       1003
+#define IDC_SECOND                      1004
+#define IDC_ZALWYK                      1005
+#define IDC_EGZ                         1006
+#define IDC_PRZEPIS                     1007
+#define IDC_WYKL                        1008
+#define IDC_CW                          1009
+#define IDC_LETTERS                     1010
+#define IDC_BUTTON1                     1011
+#define IDC_GENER_RANDOM                1011
+#define IDC_RESULT                      1012
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        313
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
