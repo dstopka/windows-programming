0e2ed1f112fbf9c2a91fde2e9b751fee2aecdc8f
dstopka <dariusz.marek.stopka@gmail.com>
2020-04-22 20:52:14 +0200

    [ex_03_dialog] Add window name

--- excercises/ex_03_dialog/ex_03_dialog.cpp
+++ excercises/ex_03_dialog/ex_03_dialog.cpp
@@ -119,6 +119,7 @@ BOOL CDialogApp::InitInstance()
 		return FALSE;
 
 	// The one and only window has been initialized, so show and update it
+	m_pMainWnd->SetWindowText( L"Dialog - Stopka Dariusz" );
 	m_pMainWnd->ShowWindow(SW_SHOW);
 	m_pMainWnd->UpdateWindow();
 	return TRUE;
