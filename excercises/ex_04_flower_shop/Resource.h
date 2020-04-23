14f32fdd556256b791b2d6a412d0528a1cbc61f6
dstopka <dariusz.marek.stopka@gmail.com>
2020-04-23 10:57:26 +0200

    [ex_04_flower_shop] Add shop dialog layout

--- excercises/ex_04_flower_shop/Resource.h
+++ excercises/ex_04_flower_shop/Resource.h
@@ -2,18 +2,28 @@
 // Microsoft Visual C++ generated include file.
 // Used by ex_04_flower_shop.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDP_OLE_INIT_FAILED			100
-#define IDR_MAINFRAME				128
-#define IDR_flowerShopTYPE				130
+#define IDD_FLOWERSHP                   8
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_flowerShopTYPE              130
+#define IDC_EDIT1                       1000
+#define IDC_DISC                        1000
+#define IDC_TULIP                       1001
+#define IDC_ROSE                        1002
+#define IDC_FREZ                        1003
+#define IDC_FOIL                        1004
+#define IDC_NONE                        1005
+#define IDC_PAPER                       1006
+#define IDC_RESULT                      1007
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
